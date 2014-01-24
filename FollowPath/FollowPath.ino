4ddca2574713248df474f2b3cf1af4efd9d0e195
Dominic Frost <frostationdevice@gmail.com>
2014-01-24 12:41:52 -0700

    blah

--- FollowPath/FollowPath.ino
+++ FollowPath/FollowPath.ino
@@ -13,6 +13,7 @@ Servo rightServo;
 Servo leftServo;
 
 int leftSensor = A0;
+int rightSensor = A1;
 
 
 void setup(){
@@ -24,15 +25,24 @@ void setup(){
 // high is white
 // low is black
 void loop(){
+  Serial.print("left sensor: "); // white: 153 black: 91
   Serial.println(analogRead(leftSensor));
-  if (analogRead(leftSensor) > 120) {
-    //turn back onto black line
-    leftServo.write(10);
-    rightServo.write(170);
-  } else {
-     //stay on course
-     leftServo.write(170);
-     rightServo.write(10); 
+  Serial.print("right sensor: "); // white: 211 black: 113
+  Serial.println(analogRead(rightSensor));
+  
+  // left sensor reads white - turn right
+  if (analogRead(leftSensor) > 125) {
+    rightServo.write(95);
+  } else { // stay on course
+    rightServo.write(85);
   }
-
+  
+  
+  // right sensor reads white - turn right
+  if (analogRead(rightSensor) > 180) {
+    leftServo.write(85);
+  } else { // stay on course
+    leftServo.write(95);
+  }
+  
 }
