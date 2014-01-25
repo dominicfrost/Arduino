f5c2d735edd233e0e03b2cf01e78c4799e89a6ed
Dominic Frost <frostationdevice@gmail.com>
2014-01-25 16:41:59 -0700

    next iteration

--- FollowPath/FollowPath.ino
+++ FollowPath/FollowPath.ino
@@ -14,7 +14,26 @@ Servo leftServo;
 
 int leftSensor = A0;
 int rightSensor = A1;
+int pingPin = 11;
 
+int inches = 0;
+int duration = 0;
+
+int rightWheelForward = 1440;
+int rightWheelStop = 1497;
+int rightWheelSlow = 1472;
+
+int leftWheelForward = 1530;
+int leftWheelStop = 1482;
+int leftWheelSlow = 1505;
+
+int rightWhiteThreshold = 200;
+int leftWhiteThreshold = 130;
+
+int leftValIn;
+int rightValIn;
+
+boolean trackState = false; // left
 
 void setup(){
   Serial.begin(9600);
@@ -25,24 +44,66 @@ void setup(){
 // high is white
 // low is black
 void loop(){
-  Serial.print("left sensor: "); // white: 153 black: 91
-  Serial.println(analogRead(leftSensor));
-  Serial.print("right sensor: "); // white: 211 black: 113
-  Serial.println(analogRead(rightSensor));
+  leftValIn = analogRead(leftSensor);
+  rightValIn = analogRead(rightSensor);
   
-  // left sensor reads white - turn right
-  if (analogRead(leftSensor) > 125) {
-    rightServo.write(95);
-  } else { // stay on course
-    rightServo.write(85);
-  }
+  Serial.print("left sensor: "); // white: 148 black: 118
+  Serial.println(leftValIn);
+  Serial.print("right sensor: "); // white: 218 black: 165
+  Serial.println(rightValIn);
   
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
   
-  // right sensor reads white - turn right
-  if (analogRead(rightSensor) > 180) {
-    leftServo.write(85);
-  } else { // stay on course
-    leftServo.write(95);
-  }
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
   
+  //only move if there is nothing in front of the PING
+//  if(inches > 2) {
+    
+//    leftValIn = 0;
+//    rightValIn = 999;
+    if (leftValIn > rightWhiteThreshold && rightValIn < rightWhiteThreshold) { 
+      // left sensor reads white, right sensor reads black - turn right
+      Serial.println("Turn Right");
+      trackState = false; // on left side of track
+      leftServo.writeMicroseconds(leftWheelForward);
+      rightServo.writeMicroseconds(rightWheelSlow);
+    } else if (rightValIn > rightWhiteThreshold && leftValIn < rightWhiteThreshold) { 
+      // right sensor reads white, left sensor reads black - turn left
+      Serial.println("Turn Left");
+      trackState = true; // on right side of track
+      leftServo.writeMicroseconds(leftWheelSlow);
+      rightServo.writeMicroseconds(rightWheelForward);
+    } else if (rightValIn > rightWhiteThreshold && leftValIn > rightWhiteThreshold) { 
+      // right sensor reads white, left sensor reads white
+      if(!trackState) { // turn right
+        leftServo.writeMicroseconds(leftWheelForward);
+        rightServo.writeMicroseconds(rightWheelStop);
+      } else { // turn left
+        leftServo.writeMicroseconds(leftWheelStop);
+        rightServo.writeMicroseconds(rightWheelForward);
+      }
+    } else if (rightValIn < rightWhiteThreshold && leftValIn < rightWhiteThreshold) { 
+      // right sensor reads black, left sensor reads black got straight
+      leftServo.writeMicroseconds(leftWheelForward);
+      rightServo.writeMicroseconds(rightWheelForward);
+    }
+//  } else {
+//      rightServo.writeMicroseconds(rightWheelStop);
+//      leftServo.writeMicroseconds(leftWheelStop);
+//  }
+  
+}
+
+int microsecondsToInches(int microseconds)
+{
+  return microseconds / 74 / 2;
 }
